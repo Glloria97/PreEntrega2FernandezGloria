c7c7f61f2e949bae3884b241b1951aa9cf0f67a6
Glloria97 <gloriabelen2013@gmail.com>
2022-09-21 19:37:23 -0300

    Segunda Preentrega Desarrollow web incluyendo bootstrap, animaciones, flex, grids, etc

---
+++ js/index.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.QueOfrecemos', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+    sr.reveal('.acts1', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+    sr.reveal('.acts2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+    sr.reveal('.valoresContenedor', {
+        duration: 3500,
+        origin: 'bottom',
+        distance: '-50px',
+     });
+
+    sr.reveal('.horarios', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+    
+     sr.reveal('.NuevasSedes', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+     sr.reveal('.DetalleSedes', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px',
+     });
